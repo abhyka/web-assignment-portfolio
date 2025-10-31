992b92f0eafc3dde2a9c8f679c23f0e2928fec3d
Halim Yalçınkaya <abh.yalcinkaya@gmail.com>
2025-10-31 20:35:27 +0100

    created content collections, adjusted the schema for slug creation, finished projects overview

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,7 +3,6 @@ import { z, defineCollection } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const projects = defineCollection({ 
-    loader: glob({ pattern: '*.md', base: './src/content/projects'}),
     schema: z.object({
         title: z.string(),
         date: z.date(),
@@ -16,7 +15,6 @@ const projects = defineCollection({
  });
  
 const fieldNotes = defineCollection({ 
-    loader: glob({ pattern: '*.md', base: './src/content/field-notes'}),
     schema: z.object({
         title: z.string(),
         date: z.date(),
